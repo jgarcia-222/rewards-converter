7f254e7d9aefb4a714e0d54e23a6d1b43af4e75a
jgarcia-222 <73655989+jgarcia-222@users.noreply.github.com>
2024-06-27 13:46:34 -0500

    created RewardValue class

--- src/main/java/RewardValue.js
+++ src/main/java/RewardValue.js
@@ -1,17 +1,19 @@
-class RewardValue
+public class RewardValue
 {
-    constructor(cashValue)
-    { this.cash = cashValue; }
-
-    constructor(milesValue)
-    { this.miles = milesValue; }
-
-    getCashValue() {
-        return (this.miles * 0.0035);
+    double cash;
+    int miles;
+    public RewardValue(cashValue)
+    {
+        this.cash = cashValue;
+        this.miles = cashValue/0.0035;
     }
 
-    getMilesValue() {
-        return (this.cash / 0.0035 );
+    public getCashValue() {
+        return RewardValue.cash;
+    }
+
+    public getMilesValue() {
+        return RewardValue.miles;
     }
 
 }
